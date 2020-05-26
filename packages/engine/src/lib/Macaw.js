de3faf316d81535307c5ac1ad3c4a9d8b22dcb30
Thomas Schoffelen <thomas@schof.co>
2020-05-26 19:33:44 +0100

    Add JSDoc comments to engine methods.

--- packages/engine/src/lib/Macaw.js
+++ packages/engine/src/lib/Macaw.js
@@ -20,12 +20,55 @@ const defaultOptions = {
   }
 };
 
+/**
+ * Constructor for the Macaw engine class.
+ *
+ * Usually, rather than initiating this class directly, you'd use the factory
+ * function `macaw`.
+ *
+ * ```js
+ * const mailer = macaw({ templatesDirectory: 'emails' });
+ * ```
+ *
+ * @param {object} options Engine options
+ * @param {string} options.templateFileExtension File extension appended to
+ *    template files - defaults to `md`.
+ * @param {string} options.templatesDirectory Directory to use to look for
+ *    template files - defaults to `emails`.
+ * @param {string} options.layoutDirectory Layouts directory name, needs to be
+ *    a subdirectory of the templatesDirectory - defaults to `layouts`.
+ * @param {object} options.storage Define this to override the default storage
+ *    engine, needs to be an object with a method `getObject` - defaults to
+ *    `fsStorage`.
+ * @param {object} options.provider Specify email provider, needs to be a
+ *    object with a method `send`, have a look at any of the included
+ *    providers (e.g. Sendgrid) for an example on how to create your own.
+ * @param {object} options.markdown Set custom options for markdown rendering
+ *    engine. See the [Showdown docs](https://github.com/showdownjs/showdown#valid-options)
+ *    for more info on valid options.
+ * @param {object} options.mjml Set custom options for the
+ *    [MJML renderer](https://github.com/mjmlio/mjml#inside-nodejs).
+ */
 class Macaw {
   constructor(options = {}) {
     this.options = { ...defaultOptions, ...options };
     this.options.storage.setOptions({ ...this.options, storage: undefined });
   }
 
+  /**
+   * Load a template from storage, and set any variables that should be made available
+   * within the template. Returns a Template instance.
+   *
+   * Note that this method is async!
+   *
+   * ```js
+   * const template = await mailer.template('newsletter', { name: 'John' });
+   * ```
+   *
+   * @param {string} templateName The name of the template to load, excluding directory name and extension
+   * @param {{[string]: any}} data An object of variables to set in the template
+   * @returns {Promise<Template>}
+   */
   async template(templateName, data) {
     return Template.load(
       `${templateName}.${this.options.templateFileExtension}`,
--- packages/engine/src/lib/Template.js
+++ packages/engine/src/lib/Template.js
@@ -4,12 +4,27 @@ const mjml2html = require("mjml");
 const Twig = require("twig");
 const twig = Twig.twig;
 
+/**
+ * Constructor for the Macaw template instance.
+ *
+ * Rather than initiating this class directly, use the `template` factory
+ * method in the `Macaw` class.
+ *
+ * @params {object} options Macaw instance options
+ * @params {object} data Variables to pass along to renderer
+ */
 class Template {
   constructor(options, data) {
     this.options = options;
     this.data = data;
   }
 
+  /**
+   * Load a template file and its layout from the storage engine.
+   *
+   * @param {string} templatePath The template path
+   * @returns {Promise<void>}
+   */
   async loadFile(templatePath) {
     const content = await this.options.storage.getItem(templatePath);
     const { attributes, body } = fm(content);
@@ -24,6 +39,11 @@ class Template {
     this.mjml = await this.options.storage.getItem(this.layoutFilePath);
   }
 
+  /**
+   * Parse the template and return raw HTML output.
+   *
+   * @returns {string} Parsed template HTML
+   */
   render() {
     const markdown = twig({ data: this.markdown }).render(this.data);
     const converter = new showdown.Converter(this.options.markdown);
@@ -48,7 +68,16 @@ class Template {
     return html;
   }
 
-  send(sendOptions) {
+  /**
+   * Send the template via the `provider` specified in the Macaw options.
+   *
+   * Every provider requires their own set of `sendOptions`, so have a look
+   * at the README file for the provider to find out what to pass along.
+   *
+   * @param {object} sendOptions Options to be passed to provider
+   * @returns {Promise<any>} Response from provider
+   */
+  async send(sendOptions) {
     if (!this.options.provider) {
       throw Error("No provider set to send email with.");
     }
@@ -62,6 +91,14 @@ class Template {
   }
 }
 
+/**
+ * Static template loading helper.
+ *
+ * @param {string} templatePath Path of the template in the storage engine
+ * @param {object} options Macaw instance options
+ * @param {object} data Variables to pass along to renderer
+ * @returns {Promise<Template>}
+ */
 Template.load = async (templatePath, options, data) => {
   const template = new Template(options, data);
   await template.loadFile(templatePath);
