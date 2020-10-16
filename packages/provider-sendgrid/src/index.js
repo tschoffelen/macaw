7e050d676965761694692cc11f0ef2b458cb0135
Thomas Schoffelen <thomas@schof.co>
2020-10-16 16:26:08 +0100

    Add categories & tests for Sendgrid provider.

--- packages/provider-sendgrid/src/index.js
+++ packages/provider-sendgrid/src/index.js
@@ -9,12 +9,21 @@ module.exports = ({ apiKey }) => {
   return {
     sendgrid,
     send: function sendViaSendgrid(options) {
-      const { data, html, ...providerOptions } = options;
+      let { data, html, category, categories, ...providerOptions } = options;
+
+      categories = [
+        ...(data.template ? [data.template] : []),
+        ...(categories || [])
+      ];
+      if (data.template && data.template.includes("-")) {
+        categories.push(data.template.split("-", 1)[0]);
+      }
 
       return sendgrid.send({
         subject: providerOptions.subject || data.subject,
         to: providerOptions.to || data.to,
         from: providerOptions.from || data.from,
+        categories,
         ...providerOptions,
         html
       });
--- packages/provider-sendgrid/tests/index.test.js
+++ packages/provider-sendgrid/tests/index.test.js
@@ -1,3 +1,63 @@
-test("placeholder", () => {
-  // TODO
+jest.mock("@sendgrid/mail");
+const sendgrid = require("@sendgrid/mail");
+
+const provider = require("../src/index");
+
+test("sets api key", () => {
+  provider({ apiKey: "aaaa" });
+  expect(sendgrid.setApiKey).toBeCalledWith("aaaa");
+});
+
+test("throws when no api key is specified", () => {
+  expect(() => provider({})).toThrow();
+});
+
+test("passes through html", () => {
+  const providerOptions = {
+    data: {},
+    html: "<p>Hello, world!</p>"
+  };
+
+  const sender = provider({ apiKey: "aaaa" });
+  sender.send(providerOptions);
+  expect(sendgrid.setApiKey).toBeCalledWith("aaaa");
+  expect(sendgrid.send).toBeCalledWith({
+    html: "<p>Hello, world!</p>",
+    categories: []
+  });
+});
+
+test("sets expected categories", () => {
+  const providerOptions = {
+    data: {
+      template: "intro-customer-email"
+    },
+    html: "<p>Hello, world!</p>"
+  };
+
+  const sender = provider({ apiKey: "aaaa" });
+  sender.send(providerOptions);
+  expect(sendgrid.setApiKey).toBeCalledWith("aaaa");
+  expect(sendgrid.send).toBeCalledWith({
+    categories: ["intro-customer-email", "intro"],
+    html: "<p>Hello, world!</p>"
+  });
+});
+
+test("sets expected custom categories", () => {
+  const providerOptions = {
+    data: {
+      template: "intro-customer"
+    },
+    categories: ["payment"],
+    html: "<p>Hello, world!</p>"
+  };
+
+  const sender = provider({ apiKey: "aaaa" });
+  sender.send(providerOptions);
+  expect(sendgrid.setApiKey).toBeCalledWith("aaaa");
+  expect(sendgrid.send).toBeCalledWith({
+    categories: ["intro-customer", "payment", "intro"],
+    html: "<p>Hello, world!</p>"
+  });
 });
