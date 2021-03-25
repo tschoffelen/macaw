8e92d3b9a86d9addd68faae1ce485b9970dfc271
Thomas Schoffelen <thomas@schof.co>
2021-03-25 18:08:55 +0000

    Add download button to preview UI.

--- packages/preview-ui/src/App.js
+++ packages/preview-ui/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, createRef } from "react";
 import { UnControlled as CodeMirror } from "react-codemirror2";
+import { saveAs } from "file-saver";
 import socketio from "socket.io-client";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/lib/codemirror.css";
@@ -13,7 +14,9 @@ import "./index.css";
 const jsonlint = require("jsonlint-mod");
 window.jsonlint = jsonlint;
 
-const socket = socketio(window.location.host.replace(":3000", ":4000"));
+const socket = socketio(
+  window.location.host.replace(":3000", ":4000").replace(":3001", ":4000")
+);
 
 let defaultData = {
   subject: "Hello, world",
@@ -26,6 +29,7 @@ function App() {
   const [initialJson, setInitialJson] = useState(defaultJson);
   const [json, setJson] = useState(defaultJson);
   const [mode, setMode] = useState("responsive");
+  const [html, setHtml] = useState("");
   const [template, setTemplate] = useState(localStorage.lastTemplate || "");
   const [templates, setTemplates] = useState([]);
   const iframe = createRef();
@@ -43,6 +47,7 @@ function App() {
   }, [template, templates]);
   useEffect(() => {
     socket.on("render", html => {
+      setHtml(html);
       if (!iframe.current) {
         return;
       }
@@ -79,6 +84,23 @@ function App() {
       // do nothing
     }
   }, [template]);
+
+  const downloadAsHtml = () => {
+    const contents = `
+      <!-- 
+      
+      Template name: ${template}
+      
+      JSON payload:
+      ${json}
+      
+      -->
+      ${html}
+    `;
+
+    const blob = new Blob([contents], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, `${template}.html`);
+  };
 
   return (
     <>
@@ -169,74 +191,74 @@ function App() {
             </div>
           </section>
           <section>
-            <h3>Preview mode</h3>
-            <div className="toggle">
-              <button
-                onClick={() => setMode("responsive")}
-                className={`responsive ${
-                  mode === "responsive" ? "active" : ""
-                }`}
-              >
-                <svg width="26" height="21">
-                  <g
-                    fill="none"
-                    fillRule="evenodd"
-                    transform="translate(-34 -18)"
-                  >
-                    <path
-                      d="M55.167 28.5h-2.334V32h-3.5v2.333h5.834V28.5zm-14-3.5h3.5v-2.333h-5.834V28.5h2.334V25zM57.5 18h-21a2.333 2.333 0 00-2.333 2.333v16.334A2.333 2.333 0 0036.5 39h21a2.333 2.333 0 002.333-2.333V20.333A2.333 2.333 0 0057.5 18zm0 18.684h-21V20.316h21v16.368z"
-                      fill="#000"
-                      fillRule="nonzero"
-                    />
-                    <path d="M33 14.5h28v28H33z" />
-                  </g>
-                </svg>
-                Responsive
+            <h3>Options</h3>
+            <div className="options">
+              <button onClick={downloadAsHtml} className="styled-button">
+                Download
               </button>
-              <button
-                onClick={() => setMode("tablet")}
-                className={`tablet ${mode === "tablet" ? "active" : ""}`}
-              >
-                <svg width="26" height="19">
-                  <g
-                    fill="none"
-                    fillRule="evenodd"
-                    transform="translate(-123 -19)"
-                  >
-                    <path d="M122 14.5h28v28h-28z" />
-                    <path
-                      d="M146.5 19.167h-21a2.333 2.333 0 00-2.333 2.333v14a2.333 2.333 0 002.333 2.333h21a2.323 2.323 0 002.322-2.333l.011-14a2.333 2.333 0 00-2.333-2.333zM144.167 35.5h-16.334v-14h16.334v14z"
-                      fill="#000"
-                      fillRule="nonzero"
-                    />
-                  </g>
-                </svg>
-                Tablet
-              </button>
-              <button
-                onClick={() => setMode("mobile")}
-                className={`mobile ${mode === "mobile" ? "active" : ""}`}
-              >
-                <svg width="16" height="27">
-                  <g
-                    fill="none"
-                    fillRule="evenodd"
-                    transform="translate(-216 -15)"
-                  >
-                    <path
-                      d="M229.083 15.667h-9.333a2.918 2.918 0 00-2.917 2.916v19.834a2.918 2.918 0 002.917 2.916h9.333A2.918 2.918 0 00232 38.417V18.583a2.918 2.918 0 00-2.917-2.916zm-4.666 24.5c-.969 0-1.75-.782-1.75-1.75 0-.969.781-1.75 1.75-1.75.968 0 1.75.781 1.75 1.75 0 .968-.782 1.75-1.75 1.75zm5.25-4.667h-10.5V19.167h10.5V35.5z"
-                      fill="#000"
-                      fillRule="nonzero"
-                    />
-                    <path d="M211 14.5h28v28h-28z" />
-                  </g>
-                </svg>
-                Mobile
-              </button>
+              <div className="toggle">
+                <button
+                  onClick={() => setMode("responsive")}
+                  className={`responsive ${
+                    mode === "responsive" ? "active" : ""
+                  }`}
+                >
+                  <svg width="26" height="21">
+                    <g
+                      fill="none"
+                      fillRule="evenodd"
+                      transform="translate(-34 -18)"
+                    >
+                      <path
+                        d="M55.167 28.5h-2.334V32h-3.5v2.333h5.834V28.5zm-14-3.5h3.5v-2.333h-5.834V28.5h2.334V25zM57.5 18h-21a2.333 2.333 0 00-2.333 2.333v16.334A2.333 2.333 0 0036.5 39h21a2.333 2.333 0 002.333-2.333V20.333A2.333 2.333 0 0057.5 18zm0 18.684h-21V20.316h21v16.368z"
+                        fill="#000"
+                        fillRule="nonzero"
+                      />
+                      <path d="M33 14.5h28v28H33z" />
+                    </g>
+                  </svg>
+                </button>
+                <button
+                  onClick={() => setMode("tablet")}
+                  className={`tablet ${mode === "tablet" ? "active" : ""}`}
+                >
+                  <svg width="26" height="19">
+                    <g
+                      fill="none"
+                      fillRule="evenodd"
+                      transform="translate(-123 -19)"
+                    >
+                      <path d="M122 14.5h28v28h-28z" />
+                      <path
+                        d="M146.5 19.167h-21a2.333 2.333 0 00-2.333 2.333v14a2.333 2.333 0 002.333 2.333h21a2.323 2.323 0 002.322-2.333l.011-14a2.333 2.333 0 00-2.333-2.333zM144.167 35.5h-16.334v-14h16.334v14z"
+                        fill="#000"
+                        fillRule="nonzero"
+                      />
+                    </g>
+                  </svg>
+                </button>
+                <button
+                  onClick={() => setMode("mobile")}
+                  className={`mobile ${mode === "mobile" ? "active" : ""}`}
+                >
+                  <svg width="16" height="27">
+                    <g
+                      fill="none"
+                      fillRule="evenodd"
+                      transform="translate(-216 -15)"
+                    >
+                      <path
+                        d="M229.083 15.667h-9.333a2.918 2.918 0 00-2.917 2.916v19.834a2.918 2.918 0 002.917 2.916h9.333A2.918 2.918 0 00232 38.417V18.583a2.918 2.918 0 00-2.917-2.916zm-4.666 24.5c-.969 0-1.75-.782-1.75-1.75 0-.969.781-1.75 1.75-1.75.968 0 1.75.781 1.75 1.75 0 .968-.782 1.75-1.75 1.75zm5.25-4.667h-10.5V19.167h10.5V35.5z"
+                        fill="#000"
+                        fillRule="nonzero"
+                      />
+                      <path d="M211 14.5h28v28h-28z" />
+                    </g>
+                  </svg>
+                </button>
+              </div>
             </div>
           </section>
-        </div>
-        <div>
           <section>
             <h3>JSON data</h3>
             <CodeMirror
